b81c87fad7de2845320818483cc391d8e792339c
duxiaodong <duxiaodong@darlin.me>
2016-06-14 18:24:42 +0800

    add babel

--- src/data.js
+++ src/data.js
@@ -9,11 +9,19 @@ var 基本信息 = new Map([
   ['生日', '1992-01-22']
 ]);
 
-var 工作经历 = new Map([]);
+var 工作经历 = new Map([
+  ['公司', '七牛'],
+  ['职位', '前端']
+]);
 
-var 技能 = new Map([]);
+var 技能 = new Map([
+  ['JavaScript', '熟悉'],
+  ['TypeScript', '了解']
+]);
 
-var 其他 = new Map([]);
+var 其他 = new Map([
+  ['爱好', '篮球']
+]);
 
 export {
   核心信息,
